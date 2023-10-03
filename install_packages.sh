7cb8a2d1ad6184d566736eafd50b24e0658747b3
Roni Björn <roni.bjorn@frozenbyte.com>
2023-10-03 09:37:14 +0300

    Map arrow keys to Caps_Lock+[i,j,k,l]

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo pacman -S base-devel curl wget unzip tar gzip npm neovim zsh git kitty ripgrep python3 zsh-syntax-highlighting python-pynvim ttf-firacode-nerd
+sudo pacman -S base-devel curl wget unzip tar gzip npm neovim zsh git kitty ripgrep python3 zsh-syntax-highlighting python-pynvim ttf-firacode-nerd xdotool spectacle i3lock i3status
