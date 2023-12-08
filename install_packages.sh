c43a2aaec115b7e7dbbf80cd9eca3fb376d66c48
Roni Björn <roni.bjorn@gmail.com>
2023-12-08 07:39:11 +0200

    Add configs for wayland/hyprland

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-sudo pacman -S base-devel curl wget unzip tar gzip npm neovim zsh git kitty ripgrep python3 zsh-syntax-highlighting python-pynvim ttf-firacode-nerd xdotool spectacle i3lock i3status xclip
+sudo pacman -S base-devel curl wget unzip tar gzip npm neovim zsh git kitty ripgrep python3 zsh-syntax-highlighting python-pynvim ttf-firacode-nerd xdotool spectacle i3lock i3status xclip hyprland hyprpaper wofi pipewire wireplumber waybar
