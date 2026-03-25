12e84377e54c06bacc5aada8c8e6cef33776dc08
Ronaattori <ronaattori@tarinapiski.com>
2026-03-25 09:32:22 +0200

    Make vim autodetect indent types

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,11 +1,14 @@
 local opts = {
 	shiftwidth = 4,
 	tabstop = 4,
-	expandtab = true,
 	wrap = false,
 	termguicolors = true,
 	number = true,
 	relativenumber = true,
+	-- Auto indenting new lines and copy the style (tab vs space) from the line above
+	autoindent = true,
+	smartindent = true,
+	copyindent = true,
 }
 
 -- Set options from table
@@ -15,12 +18,12 @@ end
 
 -- Set other options
 if not vim.g.vscode then
-    vim.cmd.colorscheme("PaperColor")
+	vim.cmd.colorscheme("PaperColor")
 end
 
 vim.diagnostic.config({
-  virtual_text = true,
-  underline = true,
-  signs = true,
+	virtual_text = true,
+	underline = true,
+	signs = true,
 })
 
